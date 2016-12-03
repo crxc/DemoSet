e06f3d98e7bf150303186119ca1f76dc9471ad9f
crxc <jdxbwsbf@gmail.com>
2016-12-03 10:42:19 +0800

    完善短信大全案例

--- demo合集/src/main/java/com/example/demo/MainActivity.java
+++ demo合集/src/main/java/com/example/demo/MainActivity.java
@@ -1,10 +1,16 @@
 package com.example.demo;
 
+import android.app.Activity;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
+import android.content.ContentResolver;
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
@@ -16,8 +22,6 @@ import android.widget.ExpandableListView;
 import android.widget.Toast;
 
 import com.example.demo.adapter.MyExpandableListAdapter;
-
-
 import com.example.demo.fragment.AnimationFragment_;
 import com.example.demo.fragment.AutoCompleteTextViewFragment_;
 import com.example.demo.fragment.ContactFragment;
@@ -50,13 +54,12 @@ public class MainActivity extends AppCompatActivity implements SmsDemoFragment.S
     ActionBarDrawerToggle actionBarDrawerToggle;
     private FragmentManager manager;
     private String smsFragmentTag ="sms";
-    private String contactInterface ="contact";
     private String smsTempTag="smsTemp";
     static {
         initData();
     }
 
-    private static void initData() {
+    static void initData() {
         App app = App.getContext();
         SharedPreferences config = app.getSharedPreferences("config", MODE_PRIVATE);
         boolean isFirstUse = config.getBoolean("isFirstUse", true);
@@ -85,7 +88,7 @@ public class MainActivity extends AppCompatActivity implements SmsDemoFragment.S
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-//    @ViewById(R.id.left_drawer)
+    //    @ViewById(R.id.left_drawer)
 //    RecyclerView rv;
     @ViewById(R.id.expanded_menu)
     ExpandableListView mExpandView;
@@ -309,17 +312,45 @@ public class MainActivity extends AppCompatActivity implements SmsDemoFragment.S
     }
 
     @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode== Activity.RESULT_OK){
+            ContentResolver resolver = getContentResolver();
+            Uri uri = data.getData();
+            Cursor cursor = resolver.query(uri, null, null, null, null);
+            if(cursor==null){
+                Toast.makeText(this, "没有查询到联系人", Toast.LENGTH_SHORT).show();
+                return;
+            }
+            cursor.moveToFirst();
+//            String name = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
+            String contactId = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts._ID));
+            Cursor phoneCursor=resolver.query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI
+                    ,null, ContactsContract.CommonDataKinds.Phone.CONTACT_ID+"="+contactId,null,null);
+            String userNumber = null;
+            if(phoneCursor==null){
+                Toast.makeText(this, "没有查询到电话", Toast.LENGTH_SHORT).show();
+                return;
+            }
+            while (phoneCursor.moveToNext()){
+                userNumber=phoneCursor.getString(phoneCursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+            }
+            cursor.close();
+            phoneCursor.close();
+            returnContactPhone(userNumber);
+        }
+    }
+
+    @Override
     public void OpenSelectContactInterface() {
-            manager.beginTransaction().hide(manager.findFragmentByTag(smsFragmentTag))
-                    .add(R.id.frame_main,new ContactFragment_(), contactInterface).commit();
+        Intent intent = new Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI);
+        startActivityForResult(intent,0);
     }
 
     @Override
     public void returnContactPhone(String phone) {
         SmsDemoFragment_ smsFragment = (SmsDemoFragment_) manager.findFragmentByTag(smsFragmentTag);
         smsFragment.setPhone(phone);
-        manager.beginTransaction().remove(manager.findFragmentByTag(contactInterface))
-                .show(smsFragment).commit();
     }
 
 
